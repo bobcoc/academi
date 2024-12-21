50baa6a77cf097e5c41e4dad4053d34f1809e73b
smshine <smshine@qq.com>
2024-12-21 16:48:30 +0800

    lang/zh_cn/theme_academi.php

---
+++ lang/zh_cn/theme_academi.php
@@ -0,0 +1,261 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Simplified Chinese lang file
+ * @package    theme_academi
+ * @copyright  2015 onwards LMSACE Dev Team (http://www.lmsace.com)
+ * @author    LMSACE Dev Team
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['about'] = '关于';
+$string['aboutus'] = '关于我们';
+$string['address'] = '地址';
+$string['autoslideshow'] = '自动播放';
+$string['autoslideshowdesc'] = '使用此选项可在页面加载后启用幻灯片自动滚动。启用后，首页幻灯片将自动切换，无需用户输入或互动。';
+$string['availablecoursetype'] = '可用课程显示样式';
+$string['availablecoursetype_desc'] = '请选择可用课程列表的显示样式。';
+$string['backToTop_status'] = "添加返回顶部按钮";
+$string['backToTop_statusdesc'] = "此选项控制是否在页面上启用返回顶部按钮。";
+$string['backgroundimage_desc'] = '要显示为网站主题背景的图片。<br><b>注意：</b>背景图片基于父主题"Boost"工作。如果在"Boost"主题中删除背景图片，它也会在我们的"Academi"主题中被删除。';
+$string['blockemptymessage'] = '<p><i class="fa fa-info-circle"></i> 此功能设置已启用但未正确配置。请及时检查并配置。</p>';
+$string['bottomCenter'] = '底部居中';
+$string['bottomLeft'] = '底部左侧';
+$string['bottomRight'] = '底部右侧';
+$string['buttonlink'] = '按钮链接';
+$string['buttontarget'] = '按钮目标';
+$string['buttontxt'] = '按钮文本';
+$string['calendar'] = '日历';
+$string['callus'] = '<span>联系电话</span>';
+$string['carousel'] = '轮播';
+$string['center'] = '居中';
+$string['centerLeft'] = '中部左侧';
+$string['centerRight'] = '中部右侧';
+$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot"><img class=img-polaroid src="academi/pix/screenshot.jpg" /><br/><p></p><h3>主题致谢</h3><p><h3>Moodle Academi主题</h3><p>本主题基于Boost Moodle主题。</p><p>作者：LMSACE开发团队<br>联系方式：info@lmsace.com<br>网站：<a href="http://www.lmsace.com">www.lmsace.com</a><br></p>';
+$string['collapse'] = '折叠';
+$string['comboListboxType'] = '组合列表显示样式';
+$string['comboListboxType_desc'] = '选择组合列表块的显示样式。';
+$string['configtitle'] = 'Academi';
+$string['contact'] = '联系';
+$string['contact_us'] = '联系我们';
+$string['container'] = '容器';
+$string['content'] = '内容';
+$string['content_desc'] = '要设置此块的内容，请以纯文本形式输入块内容，或使用"lang"前缀后跟适当的"language_key"以支持多语言。您可以在"/theme/academi/lang/en/theme_academi.php"文件中找到多语言支持的主题语言键。';
+$string['copyright_default'] = '版权所有 &copy; 2017 - 由<a href="http://lmsace.com">LMSACE.com</a>开发。基于<a href="https://moodle.org">Moodle</a>驱动';
+$string['copyright_footer'] = '版权';
+$string['custom'] = '自定义';
+$string['customcss'] = '自定义CSS';
+$string['customcssdesc'] = '您添加到此文本区域的任何CSS规则都将反映在每个页面中，便于自定义此主题。';
+$string['defaultaddress'] = '308 Negra Narrow Lane, Albeeze, New york, 87104';
+$string['defaultemailid'] = 'info@example.com';
+$string['defaultphoneno'] = '(000) 123-456';
+$string['description'] = '描述';
+$string['description_default'] = 'Lorem Ipsum是印刷和排版行业的虚拟文本';
+$string['description_desc'] = '要设置此块的描述，请以纯文本形式输入块描述，或使用"lang"前缀后跟适当的"language_key"以支持多语言。';
+$string['disable'] = '禁用';
+$string['email'] = '<span>电子邮件</span>';
+$string['emailid'] = '电子邮件';
+$string['enable'] = '启用';
+$string['expand'] = '展开';
+$string['favicon'] = '网站图标';
+$string['favicon_desc'] = '在这里，您可以上传自定义图片（.ico或.png格式），浏览器将其显示为您的Moodle网站的图标。<br>如果未上传自定义网站图标，将使用主题默认图标。';
+$string['fblock_statusdesc'] = '如果您希望在页脚部分禁用此块，请取消选中<i>激活此块</i>复选框。';
+$string['fontsize'] = '内容字体大小';
+$string['fontsize_desc'] = '更改网站内容的字体大小。';
+$string['footerbgOverlay'] = '页脚覆盖层透明度';
+$string['footerbgOverlay_desc'] = '使用此选项控制页脚块中覆盖层的透明度级别。默认值为0.4，这意味着覆盖层将有40%的不透明度，允许其后面的内容部分可见。';
+$string['footerbgimg'] = '页脚背景图片';
+$string['footerbgimgdesc'] = '如果您想在页脚添加自定义背景图片，请在此处上传。背景图片尺寸应为1345 X 517';
+$string['footerblock'] = '页脚块';
+$string['footerblockgeneral'] = '通用页脚块';
+$string['footerbtitle1default'] = '关于我们';
+$string['footerbtitle2default'] = '信息';
+$string['footerbtitle3default'] = '联系我们';
+$string['footerbtitle4default'] = '关注我们';
+$string['footerbtitledesc'] = '输入此块的标题，可以是纯文本或使用lang:language_key来支持多语言。您可以在/theme/academi/lang/en/theme_academi.php中找到主题语言键。';
+$string['footerenable'] = '启用页脚logo';
+$string['footerheading'] = '页脚';
+$string['footerlogo'] = '页脚logo';
+$string['footerlogodesc'] = '如果您想在页脚添加自定义logo，请在此处上传。图片高度应为80px，宽度合理（最小：205px）。';
+$string['footnote'] = '页脚注释';
+$string['footnotedefault'] = '<p>Lorem Ipsum是印刷和排版行业的虚拟文本。自1500年代以来，Lorem Ipsum一直是行业的标准虚拟文本，当一个未知的打印商采用了一个铅字版面并进行电子排版时，包含Lorem Ipsum段落的印张。<br><a href="#">阅读更多 &raquo;</a></p>';
+$string['footnotedesc'] = '您添加到此文本区域的任何内容都将显示在整个Moodle站点的页脚中。';
+$string['headerheading'] = '常规';
+$string['home'] = "首页";
+$string['icon'] = '图标';
+$string['iconcolor'] = '图标颜色';
+$string['icondesc'] = '输入您想使用的图标名称。图标列表在<a href="https://fontawesome.com/v4/cheatsheet/" target="_new">这里</a>。只需输入"fa-"后面的部分。';
+$string['image'] = '图片';
+$string['imagedesc'] = '为"特色元素"上传自定义图片。我们建议使用75 * 75像素的图片。';
+$string['importsettings'] = "导入其他预设设置";
+$string['importthemesettings'] = '导入{$a}设置';
+$string['info'] = "信息";
+$string['infolink'] = '信息链接';
+$string['infolink_desc'] = '您可以在此处配置自定义信息链接以供主题显示。每行包含菜单文本、链接URL（可选）、工具提示标题（可选）和语言代码或逗号分隔的代码列表（可选，仅向指定语言的用户显示该行），用管道字符分隔。例如：
+<pre>
+Moodle社区|https://moodle.org
+Moodle免费支持|https://moodle.org/support
+Moodle开发|https://moodle.org/development
+Moodle文档|http://docs.moodle.org|Moodle文档
+德语Moodle文档|http://docs.moodle.org/de|德语文档|de
+Moodle.com|http://moodle.com/
+</pre>';
+$string['infolinkdefault'] = 'Moodle社区|https://moodle.org
+Moodle免费支持|https://moodle.org/support
+Moodle开发|https://moodle.org/development
+Moodle文档|http://docs.moodle.org|Moodle文档
+Moodle.com|http://moodle.com/';
+$string['jumbotronbtnlink_desc'] = '输入URL以设置"Jumbotron"的按钮链接。';
+$string['jumbotronbtntarget_desc'] = '选择此选项，决定"Jumbotron"块中的按钮链接是在"相同"还是"新"窗口中打开。';
+$string['jumbotronbtntext_desc'] = '以纯文本或多语言支持的形式输入"Jumbotron"块第一个按钮的文本。';
+$string['jumbotronheading'] = 'Jumbotron';
+$string['knowmore'] = '了解更多';
+$string['large'] = '大';
+$string['learnanytime'] = '随时随地学习';
+$string['learnanytimedesc'] = '无论您在何处，何时想学习，都可以享受我们的免费在线课程。通过移动设备、平板电脑或台式机访问课程内容。';
+$string['loginbg'] = '登录背景图片';
+$string['loginbg_desc'] = '用作登录页面背景的图片。';
+$string['logo'] = 'Logo';
+$string['logoandsitename'] = 'Logo和网站名称';
+$string['logodesc'] = '如果您想在页眉添加自定义logo，请在此处上传。<br>图片高度应为37px，宽度合理（最小：250px）。';
+$string['media'] = '媒体';
+$string['medium'] = '中等';
+$string['moodlebased'] = 'Moodle基础';
+$string['moodledefault'] = "Moodle默认";
+$string['navstyle'] = '导航栏样式';
+$string['navstyle_desc'] = '';
+$string['newWindow'] = '新窗口';
+$string['numberofsitef'] = '网站特色数量';
+$string['numberofsitef_desc'] = '这允许您控制首页块中展示的网站特色数量。';
+$string['numberofslides'] = '幻灯片数量';
+$string['numberofslides_desc'] = '幻灯片显示器上的幻灯片数量。';
+$string['numofsocialmedia'] = '社交媒体数量';
+$string['numofsocialmediadesc'] = '这允许您管理页脚块中显示的社交媒体内容数量。';
+$string['pagesize'] = '内页内容大小';
+$string['pagesize_desc'] = '请选择一个内容大小选项，作为所有Moodle内页的主要内容大小。此选择不会影响您首页的内容大小。';
+$string['pagesizecustomval'] = '页面内容自定义值';
+$string['pagesizecustomval_desc'] = '请指定页面内容大小的自定义值。';
+$string['pcourses'] = '推荐课程';
+$string['pcoursesdesc'] = '请提供推荐课程ID，用逗号分隔。';
+$string['phone'] = "电话";
+$string['phoneno'] = '电话号码';
+$string['pluginname'] = 'Academi';
+$string['presetheading'] = '主题预设';
+$string['primarycolor'] = '主要颜色';
+$string['primarycolor_desc'] = '选择您品牌的主要颜色';
+$string['privacy:metadata'] = 'Academi主题不存储任何用户的个人数据。';
+$string['promotedcoursesheading'] = '推荐课程';
+$string['promotedtitledefault'] = '推荐课程';
+$string['promotedtitledesc'] = '请提供推荐课程块标题，可以是语言键或文本。例如：lang:display或Display';
+$string['readmore'] = '阅读更多';
+$string['region-side-post'] = '右侧';
+$string['region-side-pre'] = '左侧';
+$string['sameWindow'] = '相同窗口';
+$string['sb1_default_title'] = '多语言';
+$string['sb2_default_title'] = '在线学习';
+$string['sb3_default_title'] = '社区支持';
+$string['sb4_default_title'] = '响应式设计';
+$string['sb_default_content'] = '这是Lorem Ipsum的Photoshop版本。Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat.';
+$string['secondarycolor'] = '次要颜色';
+$string['secondarycolor_desc'] = '选择一个将在整个moodle站点用作次要颜色的颜色';
+$string['sitefblock'] = '网站特色 {$a->block}';
+$string['sitefblockicon1_default'] = 'globe';
+$string['sitefblockicon2_default'] = 'graduation-cap';
+$string['sitefblockicon3_default'] = 'bullhorn';
+$string['sitefblockicon4_default'] = 'mobile';
+$string['sitefeatures'] = '网站特色';
+$string['sitefeaturesdefault'] = '我们的网站特色';
+$string['sitename'] = '网站名称';
+$string['slideCont_full'] = '内容宽度';
+$string['slideCont_fulldesc'] = '使用此选项调整幻灯片的内容宽度。内容宽度将按主网格宽度的百分比计算。此选项的默认值为50%，这意味着内容将占用幻灯片中可用空间的一半。';
+$string['slideOverlay'] = '覆盖层透明度';
+$string['slideOverlay_desc'] = '使用此选项控制幻灯片显示中覆盖层的透明度级别。默认值为0.4，这意味着覆盖层将有40%的不透明度，允许其后面的内容部分可见。';
+$string['slideStatus'] = '状态（幻灯片 #{$a->slide}）';
+$string['slideStatus_desc'] = '要控制首页幻灯片中幻灯片的可见性，请为要隐藏的特定幻灯片选择"禁用"选项。选择"禁用"将阻止幻灯片在首页幻灯片中显示，有效地将其隐藏。';
+$string['slidebtnlink'] = '按钮URL';
+$string['slidebtnlink_desc'] = '使用此选项设置幻灯片上按钮的链接。输入您希望用户点击按钮时重定向到的URL或网址。';
+$string['slidebtntarget'] = '按钮目标';
+$string['slidebtntarget_desc'] = '通过选择此选项，当用户点击幻灯片上的按钮时，链接的内容将在单独的浏览器标签或窗口中打开，保持原始页面不变。';
+$string['slidebtntext'] = '按钮文本';
+$string['slidebtntext_desc'] = '使用此选项设置幻灯片上按钮的文本。如果您不想在幻灯片上显示按钮，请将此选项留空。输入按钮文本，可以是纯文本或使用"lang"前缀后跟适当的"language_key"以支持多语言。';
+$string['slidecaption'] = '幻灯片标题';
+$string['slidecaptiondefault'] = 'Bootstrap基础幻灯片 - {$a->slideno}';
+$string['slidecaptiondesc'] = '输入幻灯片要使用的标题文本';
+$string['slidecontent'] = '内容位置';
+$string['slidecontentdesc'] = '使用此选项确定幻灯片标题、描述和按钮在幻灯片中的位置。';
+$string['slidecontentstatus'] = '幻灯片内容';
+$string['slidecontentstatus_desc'] = '要控制首页幻灯片中幻灯片内容的可见性，请为要隐藏的特定幻灯片内容选择"禁用"选项。';
+$string['slidedesc'] = '幻灯片描述';
+$string['slidedescdefault'] = 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.';
+$string['slidedesctext'] = '输入幻灯片的描述。';
+$string['slideimage'] = '幻灯片图片';
+$string['slideimagedesc'] = '图片尺寸应为1400px X 418px。';
+$string['slideinterval'] = '暂停时间';
+$string['slideintervaldesc'] = '使用此选项调整幻灯片放映中每个幻灯片变化之间的暂停或等待时间。该值以毫秒为单位，其中1秒等于1000毫秒。';
+$string['slideno'] = '幻灯片 {$a->slide}';
+$string['slidenodesc'] = '输入幻灯片 {$a->slide} 的设置。';
+$string['slideshowdesc'] = '这将创建最多十二张幻灯片的幻灯片放映，供您宣传网站的重要元素。该放映是响应式的，图片高度根据屏幕大小设置。推荐高度为418px。宽度设置为100%，因此如果宽度大于屏幕大小，实际高度将更小。在较小的屏幕尺寸下，高度会动态减小，无需提供单独的图片。参考屏幕宽度 < 767px = 高度438px，宽度在768px和979px之间 = 高度418px，宽度 > 980px = 高度418px。如果未为幻灯片选择图片，则使用pix文件夹中的default_slide图片。';
+$string['slideshowheading'] = '首页幻灯片';
+$string['slideshowheadingsub'] = '首页幻灯片显示。';
+$string['small'] = '小';
+$string['smediastatus'] = '激活您的社交媒体';
+$string['smediastatus_desc'] = '此选项允许您控制块的可见性并有效管理页脚块中显示的社交媒体。';
+$string['socialmediadesc'] = '<strong>此块包含社交媒体</strong>';
+$string['socialmediaicon1_default'] = 'x-twitter';
+$string['socialmediaicon2_default'] = 'google-plus';
+$string['socialmediaicon3_default'] = 'pinterest-p';
+$string['socialmediaicon4_default'] = 'facebook-f';
+$string['socialmediaicon5_default'] = 'instagram';
+$string['socialmediaicon6_default'] = 'linkedin';
+$string['socialmediaicon7_default'] = 'skype';
+$string['socialmediaicon8_default'] = 'github';
+$string['socialmediaicon_desc'] = '您组织的社交媒体图标';
+$string['socialmediaiconcolor1_default'] = '#47caf6';
+$string['socialmediaiconcolor2_default'] = '#e84c3d';
+$string['socialmediaiconcolor3_default'] = '#cd2129';
+$string['socialmediaiconcolor4_default'] = '#3598dc';
+$string['socialmediaiconcolor5_default'] = '#f131b8';
+$string['socialmediaiconcolor6_default'] = '#0A66C2';
+$string['socialmediaiconcolor7_default'] = '#00aff0';
+$string['socialmediaiconcolor8_default'] = '#333';
+$string['socialmediaiconcolor_desc'] = '更改社交媒体图标的颜色';
+$string['socialmediaurl1_default'] = 'https://twitter.com/yourtwittername';
+$string['socialmediaurl2_default'] = 'https://www.google.com/+yourgoogleplusid';
+$string['socialmediaurl3_default'] = 'https://in.pinterest.com/yourpinterestname/';
+$string['socialmediaurl4_default'] = 'https://www.facebook.com/yourfacebookid';
+$string['socialmediaurl5_default'] = 'https://www.instagram.com//yourinstagramusername';
+$string['socialmediaurl6_default'] = 'https://in.linkedin.com//yourlinkedinid';
+$string['socialmediaurl7_default'] = 'https://www.skype.com/en/';
+$string['socialmediaurl8_default'] = 'https://github.com/';
+$string['socialmediaurl_desc'] = '您组织的社交媒体url';
+$string['socialmeida'] = '社交媒体 {$a->socialmedia}';
+$string['status'] = '激活块';
+$string['statusdesc'] = '此选项允许您控制块的可见性并有效管理首页上显示的内容。';
+$string['themebased'] = '主题基础';
+$string['themestyleheader'] = '页眉样式';
+$string['themestyleheader_desc'] = '为您的页眉选择任意样式，在moodle基础样式中，菜单显示在顶部页眉中。
+<br>底部页眉将被隐藏。在主题基础模式中，菜单显示在底部页眉中。';
+$string['title'] = '标题';
+$string['titledesc'] = '要设置此块的标题，请以纯文本形式输入块标题，或使用"lang"前缀后跟适当的"language_key"以支持多语言。';
+$string['toggleslideshow'] = '幻灯片显示';
+$string['toggleslideshowdesc'] = '选择您是否希望隐藏或显示幻灯片。';
+$string['topCenter'] = '顶部居中';
+$string['topLeft'] = '顶部左侧';
+$string['topRight'] = '顶部右侧';
+$string['url'] = 'URL';
+$string['urldesc'] = '输入URL以设置此网站特色 {$a->block} 标题的链接';
+$string['viewallcourses'] = '查看所有课程';
